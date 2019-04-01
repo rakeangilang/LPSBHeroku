ac165d733f07e4d9379a96f1bc96b1b89650fc36
rakeangilang <codename.rgdp@gmail.com>
2019-04-01 11:04:09 +0700

    add db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
-
+$DATABASE_URL=parse_url('postgres://etsntudpjssagr:d7e9887ebcc6fe1eb40025a3eb3241cc1228d4ca
+464ae0bb515c9b23c4f7f84d@ec2-107-20-177-161.compute-1.amazonaws.com:5432/d3lentt
+obrfafm');
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +60,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
+            //'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
