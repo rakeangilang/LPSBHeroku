f5c43fdefa98e0a0937e47681184ccd31f8a1186
rakeangilang <codename.rgdp@gmail.com>
2019-04-03 10:31:56 +0700

    benerin double quote

--- database/migrations/2019_03_05_030058_change_dimulaipada_at_pemberitahuan.php
+++ database/migrations/2019_03_05_030058_change_dimulaipada_at_pemberitahuan.php
@@ -15,7 +15,7 @@ class ChangeDimulaipadaAtPemberitahuan extends Migration
     {
         Schema::table('pemberitahuan', function (Blueprint $table) {
             //
-            $table->renameColumn("DimulaiPada", "WaktuPemberitahuan");
+            $table->renameColumn("Dilihat", "WaktuPemberitahuan");
         });
     }
 
@@ -28,7 +28,7 @@ class ChangeDimulaipadaAtPemberitahuan extends Migration
     {
         Schema::table('pemberitahuan', function (Blueprint $table) {
             //
-            $table->renameColumn('WaktuPemberitahuan', 'DimulaiPada');
+            $table->renameColumn('WaktuPemberitahuan', 'Dilihat');
         });
     }
 }
