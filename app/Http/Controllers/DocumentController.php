2eb89bf842026678c90340dd38d97e2f03d41d87
rakeangilang <codename.rgdp@gmail.com>
2019-04-16 14:52:32 +0700

    debug corrupt docx

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -40,6 +40,39 @@ class DocumentController extends Controller
     public function getTandaTerimaSampel($pes, User $user, Request $request)
     {
         try{
+            $nama = "Sutedjo Purnomo";
+        $perusahaan = "PT. Makmur Sejahtera";
+        $alamat = "Perum. Kalibaru Permai Blok C1 No.1, Cilodong, Depok";
+        $no_hp = "6281712312";
+        $email = "sutedjo1945@gmail.com";
+        $no_pesanan = "25/3/19";
+        $no_npwp = "123456789";
+        $nama_penerima = "Mochammad Suryono Oyon";
+
+        $template_path = storage_path('templates/Template_Permohonan_Analisis.docx');
+        $hasil_path = storage_path('permohonan_analisis');
+        $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($template_path);
+        $templateProcessor->setValue('NamaLengkap', $nama);
+        $templateProcessor->setValue('Perusahaan', $perusahaan);
+        $templateProcessor->setValue('Alamat', $alamat);
+        $templateProcessor->setValue('NoHP', $no_hp);
+        $templateProcessor->setValue('Email', $email);
+        $templateProcessor->setValue('NoNPWP', $no_npwp);
+        $templateProcessor->setValue('NoPesanan', $no_pesanan);
+        $templateProcessor->setValue('NamaPenerima', $nama_penerima);
+        $templateProcessor->setImageValue('TTD', array('path' => storage_path('templates/ttd.png'), 'width'=>75, 'height'=>75));
+
+        $filename = 'Hasil ' . $nama . '.docx';
+ //     Storage::put('')
+        $base_name = 'Hasil ' . $nama;
+        $hasil_path = storage_path('permohonan_analisis/'.$filename);
+        $templateProcessor->saveAs($hasil_path);
+        $headers = ['Content-Type'=>'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'Content-Disposition'=> 'attachment; filename="'.$filename.'"'];
+
+        return response()->download($hasil_path, $base_name . '.docx', [], 'inline');
+        // ahahaha
+
+
             $id_pesanan = $pes;
             $doc_name = DokumenPesanan::select('TandaTerimaSampel')->where('IDPesanan', $id_pesanan)->first()->TandaTerimaSampel;
             // Set doc name while admin's apps not yet developed
