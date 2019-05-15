9b489b30ea90bec973aadfb6fc375b4ad676735e
rakeangilang <codename.rgdp@gmail.com>
2019-05-15 18:52:45 +0700

    debug upload

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -222,7 +222,7 @@ class DocumentController extends Controller
             $all_req = $request->all();
             $id_pesanan = $pes;
 
-            if($request->hasFile()){
+            if($request->hasFile('img')){
                 //$foto = $request->file('img');
                 //$nama_foto = "ini_gambar." . $foto->getClientOriginalExtension();
                 //$img_path = $foto->storeAs('photos1', $nama_foto);
